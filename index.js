b8ab0921a918cdf003a49535be5fe0778a4bec7c
Mueni <4missmusau@gmail.com>
2021-12-06 03:19:34 +0300

    get akan name function

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
-
-
-//akan names
+// global variables
+var gender;
+var Days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var CC, YY, MM, DD;
+// akan names
 var maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
 var femaleAkanNames = ["Akosua", "Adwoa", "Abena", "Akua", "Yaa", "Afua", "Ama"]
+
+// Validation
+const DateValidation = () => {
+    if (DD == null || DD < 1 || DD > 31 || DD == undefined) {
+        alert("Invalid Date");
+        document.getElementById('date').value = null;
+        document.getElementById('date').style.border = ("1px solid red");
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
+
+const MonthValidation = () => {
+    if (MM == null || MM < 1 || MM > 12 || MM == undefined) {
+        alert("Invalid Date");
+        document.getElementById('month').value = null;
+        document.getElementById('month').style.border = ("1px solid red");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
 
 // Calculate day of week formula
 
 const DayOfWeek = () => {
-    var century = parseInt(document.getElementById("century").value);
-    var year = parseInt(document.getElementById("year").value);
-    var month = parseInt(document.getElementById("month").value);
-    var dayOfMonth = parseInt(document.getElementById("date").value);
+    CC = parseInt(document.getElementById("century").value); //century
+    YY = parseInt(document.getElementById("year").value); // year
+    MM = parseInt(document.getElementById("month").value); // month
+    DD = parseInt(document.getElementById("date").value); // date of the month
+    
+    var dateValidation = DateValidation();
+    var monthValidation = MonthValidation();
 
-    var day = (((century / 4) - 2 * century - 1) + ((5 * year / 4)) + ((26 * (month + 1) / 10)) + dayOfMonth) % 7;
-    console.log(Math.floor(day));
+    if(dateValidation == true && monthValidation == true){
+        var day = (((CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
+        console.log(Math.floor(day));
+    
+        return (Math.floor(day));
+    }
+    else {
+        return false
+    }
 
-    return (Math.floor(day));
 }
 
+//gender Function
 const GetGender = () => {
-    var gender;
     var selectedGender = document.querySelector('input[name="gender"]:checked').value;
     
     if(selectedGender == "male") {
@@ -32,9 +70,30 @@ const GetGender = () => {
 
 }
 
+
 const GetYourAkan = () => {
     var dayOfWeek = DayOfWeek();
     GetGender();
+
+    var maleName = maleAkanNames[dayOfWeek];
+    var bornDay = Days[dayOfWeek];
+
+    var femaleName = femaleAkanNames[dayOfWeek];
+
+    //comparing days and gender to assign akan names
+    if(dayOfWeek == undefined || dayOfWeek == null || dayOfWeek == false) {
+        document.getElementById("bornDay").innerHTML = null;
+        document.getElementById("akanName").innerHTML = "Not able to show akan name now kindly check if fields are filled correctly"
+    }
+    else {
+        if(gender == "female") {
+            document.getElementById("bornDay").innerHTML = bornDay;
+            document.getElementById("akanName").innerHTML = femaleName;
+        } else {
+            document.getElementById("bornDay").innerHTML = bornDay;
+            document.getElementById("akanName").innerHTML = maleName; 
+        }
+    }
 
 
 }
